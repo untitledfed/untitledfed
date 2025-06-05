f1654412768e1b649f91592cbeecef399fb300df
GewoonWeerNiet <el128549@leerling.eligant.nl>
2025-06-05 15:30:44 +0200

    Delete unicomuc directory

--- unicomuc/js/functions.js
+++
@@ -1,8 +0,0 @@
-function downloadFile() {
-  const link = document.createElement('a');
-  link.href = '../../files/uc-extension-v1.0.zip';
-  link.download = 'uc-extension-v1.0.zip';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
